f6fa22a1a463f3d636b2bbbfe8def04c4a5971c2
Abhishek Walia <abhishek@confluent.io>
2021-08-03 21:04:09 -0700

    Improve Constant naming.

--- kafkamanager/topicmanager/constants.go
+++ kafkamanager/topicmanager/constants.go
@@ -1,39 +1,71 @@
 package topicmanager
 
-/* 	Set up a new constant for possible topic management Function
-to standardize action to be performed.
-*/
-type TopicManagementFunctionType int
-
-const (
-	CREATE_TOPIC TopicManagementFunctionType = iota
-	MODIFY_TOPIC
-	DELETE_TOPIC
-	ALTER_PARTITION_REQUEST
+type (
+	/* 	Set up a new constant for possible topic management Function
+	to standardize action to be performed.
+	*/
+	TopicManagementType int
+	/*	Set up constants for topic current status.
+	 */
+	StatusType int
 )
 
-func (r TopicManagementFunctionType) String() string {
-	return [...]string{"CREATE_TOPIC", "MODIFY_TOPIC", "DELETE_TOPIC", "ALTER_PARTITION_REQUEST"}[r]
+const (
+	TopicManagementType_UNKNOWN TopicManagementType = iota
+	TopicManagementType_CREATE_TOPIC
+	TopicManagementType_MODIFY_TOPIC
+	TopicManagementType_DELETE_TOPIC
+	TopicManagementType_ALTER_PARTITION_REQUEST
+)
+
+func (a *TopicManagementType) String() string {
+	mapping := map[TopicManagementType]string{
+		TopicManagementType_UNKNOWN:                 "UNKNOWN",
+		TopicManagementType_CREATE_TOPIC:            "CREATE_TOPIC",
+		TopicManagementType_MODIFY_TOPIC:            "MODIFY_TOPIC",
+		TopicManagementType_DELETE_TOPIC:            "DELETE_TOPIC",
+		TopicManagementType_ALTER_PARTITION_REQUEST: "ALTER_PARTITION_REQUEST",
+	}
+	s, ok := mapping[*a]
+	if !ok {
+		s = mapping[TopicManagementType_UNKNOWN]
+	}
+	return s
 }
 
-/*	Set up constants for topic current status.
- */
-type StatusType int
-
 const (
-	NOT_CREATED StatusType = iota
-	CREATING
-	CREATED
-	NOT_DELETED
-	DELETING
-	DELETED
-	MODIFYING
-	MODIFIED
-	NOT_MODIFIED
-	PARTITION_ALTERED_SUCCESSFULLY
-	PARTITION_NOT_ALTERED
+	StatusType_UNKNOWN StatusType = iota
+	StatusType_NOT_CREATED
+	StatusType_CREATING
+	StatusType_CREATED
+	StatusType_NOT_DELETED
+	StatusType_DELETING
+	StatusType_DELETED
+	StatusType_NOT_MODIFIED
+	StatusType_MODIFYING
+	StatusType_MODIFIED
+	StatusType_PARTITION_NOT_ALTERED
+	StatusType_PARTITION_ALTERED_SUCCESSFULLY
 )
 
-func (r StatusType) String() string {
-	return [...]string{"NOT_CREATED", "CREATING", "CREATED", "NOT_DELETED", "DELETING", "DELETED", "MODIFYING", "MODIFIED", "NOT_MODIFIED", "PARTITION_ALTERED_SUCCESSFULLY", "PARTITION_NOT_ALTERED"}[r]
+func (a *StatusType) String() string {
+	mapping := map[StatusType]string{
+		StatusType_UNKNOWN:                        "UNKNOWN",
+		StatusType_NOT_CREATED:                    "NOT_CREATED",
+		StatusType_CREATING:                       "CREATING",
+		StatusType_CREATED:                        "CREATED",
+		StatusType_NOT_DELETED:                    "NOT_DELETED",
+		StatusType_DELETING:                       "DELETING",
+		StatusType_DELETED:                        "DELETED",
+		StatusType_NOT_MODIFIED:                   "NOT_MODIFIED",
+		StatusType_MODIFYING:                      "MODIFYING",
+		StatusType_MODIFIED:                       "MODIFIED",
+		StatusType_PARTITION_NOT_ALTERED:          "PARTITION_NOT_ALTERED",
+		StatusType_PARTITION_ALTERED_SUCCESSFULLY: "PARTITION_ALTERED_SUCCESSFULLY",
+	}
+	s, ok := mapping[*a]
+	if !ok {
+		s = mapping[StatusType_UNKNOWN]
+	}
+	return s
 }
