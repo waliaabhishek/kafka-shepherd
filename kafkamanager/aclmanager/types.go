1cd8d5e4d219a16fb9be139a850654369593c8ff
Abhishek Walia <abhishek@confluent.io>
2021-08-03 21:03:49 -0700

    Remove un-necessary types

--- kafkamanager/aclmanager/types.go
+++ kafkamanager/aclmanager/types.go
@@ -1,45 +1,13 @@
 package aclmanager
 
-import (
-	"fmt"
-	ksinternal "shepherd/internal"
-	"strings"
-)
-
 // Any ACL Manager will need to implement this interface.
 type ACLManager interface {
-	GetACLListFromKafkaCluster()
-	// This function takes in the input and provides all the necessary permissions
-	// to the principal on a specific topic for data production.
-	SetupProducerACLs(res *ACLResourceBundleRequest)
-	SetupConsumerACLs(res *ACLResourceBundleRequest)
-	SetupSourceConnectorACLs(res *ACLResourceBundleRequest)
-	SetupSinkConnectorACLs(res *ACLResourceBundleRequest)
-	// validateResourceBundle()
-}
-
-type ACLResourceBundleRequest struct {
-	Principal   *string
-	TopicName   *[]string
-	Hostname    *[]string
-	Operation   *ksinternal.ShepherdClientType
-	respChannel *chan ACLExecutionResponse
-}
-
-func (in ACLResourceBundleRequest) String() string {
-	return fmt.Sprintf("Operation Type: [%s],\tPrincipals: [%s],\tTopicNames: [%s],\tHostNames: [%s]",
-		in.Operation.String(), strings.Join(*in.Principal, ","), strings.Join(*in.TopicName, ","), strings.Join(*in.Hostname, ","))
-}
-
-type ACLExecutionRequest struct {
-	Principal string
-	TopicName string
-	HostName  string
-	Operation *ksinternal.ShepherdClientType
-}
-
-type ACLExecutionResponse struct {
-	isSuccess      bool
-	requestDetails ACLExecutionRequest
-	err            error
+	// GetACLListFromKafkaCluster()
+	// // This function takes in the input and provides all the necessary permissions
+	// // to the principal on a specific topic for data production.
+	// SetupProducerACLs(res *ACLResourceBundleRequest)
+	// SetupConsumerACLs(res *ACLResourceBundleRequest)
+	// SetupSourceConnectorACLs(res *ACLResourceBundleRequest)
+	// SetupSinkConnectorACLs(res *ACLResourceBundleRequest)
+	// // validateResourceBundle()
 }
