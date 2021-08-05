8b522d82bfc83dff90071d1653dd8052c27fb6e9
Abhishek Walia <abhishek@confluent.io>
2021-08-04 21:41:36 -0700

    Add Create & Delete ACL functionality

--- kafkamanager/aclmanager/aclmanagement.go
+++ kafkamanager/aclmanager/aclmanagement.go
@@ -1,10 +1,10 @@
 package aclmanager
 
 import (
-	"fmt"
 	ksinternal "shepherd/internal"
 	kafkamanager "shepherd/kafkamanager"
 	"sync"
+	"time"
 
 	"github.com/Shopify/sarama"
 	"go.uber.org/zap"
@@ -12,11 +12,15 @@ import (
 
 var sca *sarama.ClusterAdmin
 var logger *zap.SugaredLogger
+var aclMappings ksinternal.ACLMapping
+var lastUpdateTime int64
 
 func init() {
 	temp := kafkamanager.GetAdminConnection().(sarama.ClusterAdmin)
 	sca = &temp
 	logger = ksinternal.GetLogger()
+	aclMappings = GetACLListInKafkaACLFormat()
+	lastUpdateTime = time.Now().Unix()
 }
 
 func GetACLListInKafkaACLFormat() ksinternal.ACLMapping {
@@ -52,137 +56,146 @@ func printACLDetails(in sarama.ResourceAcls, mapping ksinternal.ACLMapping, wg *
 				"Permission Type", v.PermissionType.String(),
 			)
 			mapping.Append(ksinternal.ACLDetails{
-				ResourceType: sarama2ShepherdResourceTypeConversion[in.Resource.ResourceType],
+				ResourceType: sarama2KafkaResourceTypeConversion[in.Resource.ResourceType],
 				ResourceName: in.Resource.ResourceName,
-				PatternType:  sarama2ShepherdPatternTypeConversion[in.Resource.ResourcePatternType],
+				PatternType:  sarama2KafkaPatternTypeConversion[in.Resource.ResourcePatternType],
 				Principal:    v.Principal,
-				Operation:    sarama2ShepherdACLOperationConversion[v.Operation],
+				Operation:    sarama2KafkaACLOperationConversion[v.Operation],
 				Hostname:     v.Host,
 			}, nil)
 		}
 	}
 }
 
-// func (kam *kafkaACLManager) SetupProducerACLs(in *ACLResourceBundleRequest) {
-
-// 	if in.Principal == nil {
-// 		logger.Warnw("No Principal value is provided. Skipping ACL set up.",
-// 			"ACL Resource Bundle:", in)
-// 		return
-// 	}
-
-// 	if in.TopicName == nil {
-// 		logger.Warnw("No TopicNames provided. Skipping ACL set up.",
-// 			"ACL Resource Bundle:", in)
-// 		return
-// 	}
-
-// 	if in.Operation == nil {
-// 		logger.Warnw("No Operation Type provided. Skipping ACL set up.",
-// 			"ACL Resource Bundle:", in)
-// 		return
-// 	}
-
-// 	var wg sync.WaitGroup
-
-// 	for _, p := range *in.Principal {
-// 		for _, t := range *in.TopicName {
-// 			if in.Hostname != nil {
-// 				for _, h := range *in.Hostname {
-// 					wg.Add(1)
-// 					go kam.executeACLRequest(ACLExecutionRequest{
-// 						Principal: p,
-// 						TopicName: t,
-// 						HostName:  h,
-// 						Operation: in.Operation,
-// 					}, &wg)
-// 				}
-// 			} else {
-// 				wg.Add(1)
-// 				go kam.executeACLRequest(ACLExecutionRequest{
-// 					Principal: p,
-// 					TopicName: t,
-// 					HostName:  "",
-// 					Operation: in.Operation,
-// 				}, &wg)
-// 			}
-// 		}
-// 	}
-// 	wg.Wait()
-// }
-
-// func (kam *kafkaACLManager) executeACLRequest(in ACLExecutionRequest, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-// 	a, r := in.renderACLExecutionObjects()
-// 	for _, acl := range a {
-// 		err := (*kam.sca).CreateACL(r, acl)
-// 		if err != nil {
-// 			logger.Warnw("Was not able to create the ACL.",
-// 				"Resource Details", r,
-// 				"ACL Type", acl,
-// 				"Error", err)
-// 		} else {
-// 			logger.Infow("Successfully created ACL.",
-// 				"Resource Details", r,
-// 				"ACL Type", acl,
-// 				"Error", err)
-// 		}
-// 	}
-// }
-
-func renderACLExecutionObjects() ([]sarama.Acl, sarama.Resource) {
-
-	rsc := sarama.Resource{
-		ResourceType:        sarama.AclResourceTopic,
-		ResourceName:        in.TopicName,
-		ResourcePatternType: sarama.AclPatternAny,
+func refreshACLMetadata(forceRefresh bool) {
+	if time.Now().Unix()-lastUpdateTime > 30 || forceRefresh {
+		aclMappings = GetACLListInKafkaACLFormat()
 	}
-	var acl []sarama.Acl
+}
 
-	switch *in.Operation {
-	case ksinternal.ShepherdClientType_PRODUCER:
-		acl = []sarama.Acl{
-			{
-				Principal:      in.Principal,
-				Operation:      sarama.AclOperationWrite,
-				PermissionType: sarama.AclPermissionAllow,
-			},
-			{
-				Principal:      in.Principal,
-				Operation:      sarama.AclOperationDescribe,
-				PermissionType: sarama.AclPermissionAllow,
-			},
-		}
-	case ksinternal.ShepherdClientType_CONSUMER:
-		acl = []sarama.Acl{
-			{
-				Principal:      in.Principal,
-				Operation:      sarama.AclOperationRead,
-				PermissionType: sarama.AclPermissionAllow,
-			},
-			{
-				Principal:      in.Principal,
-				Operation:      sarama.AclOperationDescribe,
-				PermissionType: sarama.AclPermissionAllow,
-			},
-		}
-	case ksinternal.ShepherdClientType_SOURCE_CONNECTOR:
-	case ksinternal.ShepherdClientType_SINK_CONNECTOR:
-	case ksinternal.ShepherdClientType_STREAM_READ:
-	case ksinternal.ShepherdClientType_STREAM_WRITE:
-	case ksinternal.ShepherdClientType_KSQL:
+func ExecuteRequests(requestType ACLManagementType) {
+	refreshACLMetadata(false)
+	switch requestType {
+	case ACLManagementType_CREATE_ACL:
+		createStream := ksinternal.FindNonExistentACLsInCluster(&aclMappings, ksinternal.KafkaACLOperation_ANY)
+		createACLs(createStream)
+	case ACLManagementType_DELETE_ACL:
+		deleteStream := ksinternal.FindNonExistentACLsInConfig(&aclMappings, ksinternal.KafkaACLOperation_ANY)
+		deleteACLs(deleteStream)
+	default:
+		logger.Errorw("Wrong Execution Mode selected.",
+			"Mode Provided", requestType.String())
 	}
-	return acl, rsc
 }
 
-func (kam *kafkaACLManager) SetupConsumerACLs(res *ACLResourceBundleRequest) {
-	fmt.Println("Setup the permissions for consumption")
+func createACLs(in ksinternal.ACLStreamChannels) {
+	runLoop := true
+	var wg sync.WaitGroup
+	f := func(key ksinternal.ACLDetails, val interface{}) {
+		defer wg.Done()
+		r := sarama.Resource{
+			ResourceType:        kafka2SaramaResourceTypeConversion[key.ResourceType],
+			ResourceName:        key.ResourceName,
+			ResourcePatternType: kafka2SaramaPatternTypeConversion[key.PatternType],
+		}
+		a := sarama.Acl{
+			Principal:      key.Principal,
+			Host:           key.Hostname,
+			Operation:      kafka2SaramaACLOperationConversion[key.Operation],
+			PermissionType: sarama.AclPermissionAllow,
+		}
+		err := (*sca).CreateACL(r, a)
+		if err != nil {
+			logger.Warnw("Was not able to create the ACL.",
+				"Resource Details", r.ResourceName,
+				"ACL Type", a.Operation,
+				"Error", err)
+		} else {
+			logger.Infow("Successfully created ACL.",
+				"Resource Details", r.ResourceName,
+				"ACL Type", a.Operation,
+				"Error", err)
+		}
+	}
+	for runLoop {
+		select {
+		case out := <-in.SChannel:
+			for k, v := range out {
+				// Do what is need for acl execution
+				wg.Add(1)
+				go f(k, v)
+			}
+		case <-in.FChannel:
+			// Do Nothing
+		case <-in.Finished:
+			runLoop = false
+			wg.Wait()
+		}
+	}
+
 }
 
-func (kam *kafkaACLManager) SetupSourceConnectorACLs(res *ACLResourceBundleRequest) {
-	fmt.Println("Setup the permissions for Source Connector")
+func deleteACLs(in ksinternal.ACLStreamChannels) {
+	runLoop := true
+	var wg sync.WaitGroup
+	f := func(key ksinternal.ACLDetails, val interface{}) {
+		defer wg.Done()
+		filter := sarama.AclFilter{
+			ResourceType:              kafka2SaramaResourceTypeConversion[key.ResourceType],
+			ResourcePatternTypeFilter: kafka2SaramaPatternTypeConversion[key.PatternType],
+			PermissionType:            sarama.AclPermissionAllow,
+			Operation:                 kafka2SaramaACLOperationConversion[key.Operation],
+			Version:                   1,
+		}
+		match, err := (*sca).DeleteACL(filter, false)
+		if err != nil {
+			logger.Warnw("Was not able to create the ACL.",
+				"Resource Details", filter.ResourceName,
+				"ACL Operation Type", filter.Operation,
+				"Error", err)
+		} else {
+			logger.Infow("Successfully created ACL.",
+				"Resource Details", filter.ResourceName,
+				"ACL Operation Type", filter.Operation,
+				"Matched Object Resource Details", match)
+		}
+	}
+	for runLoop {
+		select {
+		case out := <-in.SChannel:
+			for k, v := range out {
+				// Do what is need for acl execution
+				wg.Add(1)
+				go f(k, v)
+			}
+		case <-in.FChannel:
+			// Do Nothing
+		case <-in.Finished:
+			runLoop = false
+			wg.Wait()
+		}
+	}
+
 }
 
-func (kam *kafkaACLManager) SetupSinkConnectorACLs(res *ACLResourceBundleRequest) {
-	fmt.Println("Setup the permissions for Sink Connector")
-}
+// rsc := sarama.Resource{
+// 	ResourceType:        sarama.AclResourceTopic,
+// 	ResourceName:        in.TopicName,
+// 	ResourcePatternType: sarama.AclPatternAny,
+// }
+// var acl []sarama.Acl
+
+// switch *in.Operation {
+// case ksinternal.ShepherdClientType_PRODUCER:
+// 	acl = []sarama.Acl{
+// 		{
+// 			Principal:      in.Principal,
+// 			Operation:      sarama.AclOperationWrite,
+// 			PermissionType: sarama.AclPermissionAllow,
+// 		},
+// 		{
+// 			Principal:      in.Principal,
+// 			Operation:      sarama.AclOperationDescribe,
+// 			PermissionType: sarama.AclPermissionAllow,
+// 		},
+// 	}
