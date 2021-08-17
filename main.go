b2b6dac7128529ab515895af290dd42a9e0b4228
Abhishek Walia <abhishek@confluent.io>
2021-08-16 18:00:47 -0700

    Added Workflow execution

--- main.go
+++ main.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"flag"
-
-	kscm "github.com/waliaabhishek/kafka-shepherd/kafkamanager"
-
-	ksam "github.com/waliaabhishek/kafka-shepherd/kafkamanager/aclmanager"
-
-	kstm "github.com/waliaabhishek/kafka-shepherd/kafkamanager/topicmanager"
-
-	ksmisc "github.com/waliaabhishek/kafka-shepherd/misc"
+	workflow "github.com/waliaabhishek/kafka-shepherd/workflowmanagers"
 )
 
 func main() {
-	ksmisc.DottedLineOutput("Starting New Run", "=", 80)
-	flag.Parse()
-	defer kscm.CloseAdminConnection()
-
-	kstm.ExecuteRequests(1, kstm.TopicManagementType_CREATE_TOPIC)
-
-	kstm.ExecuteRequests(1, kstm.TopicManagementType_MODIFY_TOPIC)
-
-	kstm.ExecuteRequests(1, kstm.TopicManagementType_DELETE_CONFIG_TOPIC)
-
-	kstm.ExecuteRequests(1, kstm.TopicManagementType_DELETE_UNKNOWN_TOPIC)
-
-	ksam.ExecuteRequests(ksam.ACLManagementType_LIST_CLUSTER_ACL)
-	ksam.ExecuteRequests(ksam.ACLManagementType_LIST_CONFIG_ACL)
-	ksam.ExecuteRequests(ksam.ACLManagementType_CREATE_ACL)
-	ksam.ExecuteRequests(ksam.ACLManagementType_DELETE_UNKNOWN_ACL)
-	ksam.ExecuteRequests(ksam.ACLManagementType_DELETE_CONFIG_ACL)
+	workflow.ExecuteTopicManagementWorkflow(true, true, true)
+	workflow.ExecuteACLManagementWorkflow(true, true)
+	workflow.DeleteShepherdTopics(true)
+	workflow.DeleteShepherdACLs(true)
 }
-
-// TODO: Get the list of ACL's from UTM List
 
 // TODO: Generate the Baseline YAML files from pre-existing clusters.
 
